fefabd358c21a4a48fdaa920e84dca4e34263bc7
LaiWenqian <lai.wq@foxmail.com>
2017-03-19 22:36:09 +0800

    lwq update

--- week#2/LWQ.py
+++ week#2/LWQ.py
@@ -1,33 +1,45 @@
 global array
 global num
-def recursion(n,n_first):
-	
+def recursion(n,n_first):	
 	global num
 	num = num + 1
-	if (num > len(array)):
-		return False
 	n_input = (n + array[n])%len(array)
-	if(n_input == n):
-		return False
-	elif(n == n_first):
-		return True
-	elif(array[n_input]*array[n]>0):
-		recursion(n_input,n_first)
+	if(n_input == n or array[n_input]*array[n]<0):
+		return -1
+	elif (n_input == n_first or num > len(array) - n_first):
+		return -2
+	
 	else:
-		return False
+		return recursion(n_input,n_first)
 
 
-def aaa(n,na):
-	if (n == na):
+def loop(n,n_first):
+        global num
+        num = num + 1
+        n_input = (n + array[n])%len(array)
+        if(n_input == n or array[n_input]*array[n]<0):
+                return -1
+        elif (n_input == n_first or num > len(array)-n_first):
+                return -2
+        else:
+                return n_input
 
-		return True
 
 if __name__ == "__main__":
         array = input('input an array:')
 	global num
-	for i in range(len(array)):	
-		num = 0;
-		loop = recursion(i,i)
-		if(loop):
-			print 'true'
+        for i in range(len(array)):                
+		isloop = i
+		num = 0
+		#isloop = recursion(i,i)
+		while(isloop >=0):
+                	isloop = loop(isloop,i)
+		if (isloop == -1 ): continue
+		elif (isloop == -2): 
+			print 'true'	
 			break
+        if(isloop == -1):
+                print 'false'
+
+
+
